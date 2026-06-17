0210229ae07bb1d1029ab02ea7c8ebb40154710f
Daniel Kersten <daniel@dev-kersten.de>
2026-06-17 19:59:43 +0200

    feat: added admin panels

--- backend/auth_app/admin.py
+++ backend/auth_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from auth_app.models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email', 'username', 'is_staff', 'created_at')
+    list_filter = ('is_staff', 'is_superuser', 'created_at')
+    search_fields = ('email', 'username')
+    ordering = ('-created_at',)
+    
+    fieldsets = UserAdmin.fieldsets + (
+        ('Timestamps', {'fields': ('created_at', 'updated_at')}),
+    )
+    readonly_fields = ('created_at', 'updated_at')
--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from quiz.models import Quiz, Question, Answer
 
-# Register your models here.
+class AnswerInline(admin.TabularInline):
+    """Display answers inline within question form"""
+    model = Answer
+    extra = 4
+    fields = ('answer_text', 'is_correct', 'order')
+
+
+class QuestionInline(admin.StackedInline):
+    """Display questions inline within quiz form"""
+    model = Question
+    extra = 0
+    show_change_link = True
+    fields = ('question_text', 'difficulty', 'order')
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'question_count', 'created_at')
+    list_filter = ('created_at', 'user')
+    search_fields = ('title', 'youtube_url')
+    readonly_fields = ('created_at', 'updated_at', 'transcription')
+    inlines = [QuestionInline]
+    
+    def question_count(self, obj):
+        return obj.questions.count()
+    question_count.short_description = 'Questions'
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text_short', 'quiz', 'difficulty', 'order')
+    list_filter = ('difficulty', 'quiz')
+    search_fields = ('question_text',)
+    inlines = [AnswerInline]
+    
+    def question_text_short(self, obj):
+        return obj.question_text[:75] + '...' if len(obj.question_text) > 75 else obj.question_text
+    question_text_short.short_description = 'Question'
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('answer_text', 'question', 'is_correct', 'order')
+    list_filter = ('is_correct', 'question__quiz')
+    search_fields = ('answer_text',)
